3cc9e6157d52e83c6a821a713a5692c70f46e71a
j040n3t0 <joaojose.ti@gmail.com>
2019-10-01 16:01:07 -0300

    Updates Files

--- pie/piechart-report.py
+++ pie/piechart-report.py
@@ -13,25 +13,31 @@ time.sleep(1)
 print "[*] Abrindo a visualização..."
 time.sleep(1)
 
-def createGraph(a,b,c,d): 
+def createGraph(a,b,c,d,filename): 
 	labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
 	values = [a,b,c,d]
 
 	data = go.Pie(labels=labels, values=values)
 
 	py.plot({"data": data, \
-	        "layout": go.Layout(title="Relatório de Malwares | Created by: João Neto")}, \
-	        filename="graph.html", \
+	        "layout": go.Layout(title="Relatório de Malwares | Created by: João Neto"), \
+
+	        }, \
+	        #},
+	        filename=filename, \
 	        auto_open=False)
 
 print "Criando o 1º gráfico..."
-createGraph(4500, 2500, 1053, 500)
-time.sleep(5)
+createGraph(5000, 2000, 1053, 500,"graph.html")
 print "Criando o 2º gráfico..."
-createGraph(3500, 4500, 1753, 1000)
+createGraph(4500, 2500, 1753, 1000,"graph3.html")
+"""
+time.sleep(5)
+
 time.sleep(5)
 print "Criando o 3º gráfico..."
 createGraph(4000, 3300, 1953, 1500)
 time.sleep(5)
 print "Criando o ultimo gráfico..."
 createGraph(3700, 4100, 1453, 2000)
+"""
